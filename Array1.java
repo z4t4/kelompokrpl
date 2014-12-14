bb8e401fb5a9dd319ef6a9a3d4e1b05b05f0c4c6
n4dr4 <nadraririruru@gmail.com>
2014-12-14 18:11:04 +0700

    hayu kerja kelompok

---
+++ Array1.java
@@ -0,0 +1,11 @@
+class Array1{
+	public static void main(String[] args)
+	{
+		int[] intbil=new bil{2,3,6,9};
+		
+		for(int i=0; i<bil.length; i++)
+			{
+			System.out.print(intbil[]);
+			}
+		}
+	}
